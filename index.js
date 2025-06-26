444978fa717fb582343b7fc54d0ee0c6c66b31fc
Lucas Beiknejad <lucas@beiknejad.dk>
2025-06-26 13:21:56 +0200

    finish

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 import { TuyaContext  } from '@tuya/tuya-connector-nodejs';
-
-let on = true
+import 'dotenv/config'
 
 const context = new TuyaContext({
   baseUrl: 'https://openapi.tuyaeu.com',
-  accessKey: 'kjhsqngs3h4y8etwy8jt',
-  secretKey: '10081f1482ec476290c4bf95f33e8ab7',
+  accessKey: process.env.ACCESS_KEY,
+  secretKey: process.env.ACCESS_SECRET
 });
 
 const device_id = 'bf8f7ee9806fbd01e8edr7'
 
-command = (x) => await context.request({
+const command = async(x) => await context.request({
   path: `/v2.0/cloud/thing/${device_id}/shadow/properties/issue`,
   method: 'POST',
-  body: {properties: '{\"switch_1\":true}'}
+  body: {properties: `{\"switch_1\":${ x }}`}
 });
 
-async function switch() {
-  command(on ? false : true)
-  on = !on
-  command(on ? false : true)
+async function onOff() {
+  command(false)
 
-  setTimeout(switch, 600000)
+  setTimeout(() => { command(true)}, 8 * 1000)
+
+  setTimeout(() => { onOff() }, 30 * 1000)
 }
+
+onOff()
